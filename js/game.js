572bf95d24fbcc07ae12a9f74d78376ce9e53c20
Heather Olcot <holcot@505games.com>
2021-04-19 11:42:26 +0100

    added backgrounds and fixed canvas width

--- js/game.js
+++ js/game.js
@@ -1,28 +1,59 @@
 import Player from "/maggie-bird/js/player.js";
 import Input from "/maggie-bird/js/input.js";
+import { handleObstacles } from "/maggie-bird/js/obstacles.js";
+import { Background, handleBackgrounds } from "/maggie-bird/js/backgrounds.js";
 
 const GAMESTATUS = {
-    RUNNING : 0,
-    PAUSED : 1,
-    GAMEOVER : 2,
-    START : 3
+    RUNNING: 0,
+    PAUSED: 1,
+    GAMEOVER: 2,
+    START: 3
 }
 
+//Background images
+const skyBackgroundImg = new Image();
+skyBackgroundImg.src = 'img/bg/background_sky.jpg';
+const treesBackgroundImg = new Image();
+treesBackgroundImg.src = 'img/bg/background_trees.png';
+const fartreesBackgroundImg = new Image();
+fartreesBackgroundImg.src = 'img/bg/far_trees.png';
+const mountainsBackgroundImg = new Image();
+mountainsBackgroundImg.src = 'img/bg/background_mountains.png';
+const treesForegroundImg = new Image();
+treesForegroundImg.src = 'img/bg/foreground_trees.png';
+
 export default class Game {
-    constructor(canvas) {
-        this.canvas = canvas,
-        this.player = new Player(this),
-        this.input = new Input(this),
-        this.elevate = false,
-        this.status = GAMESTATUS.RUNNING
+    constructor(canvas, ctx) {
+        this.canvas = canvas;
+        this.ctx = ctx;
+        this.player = new Player(this);
+        this.input = new Input(this);
+        this.elevate = false;
+        this.status = GAMESTATUS.RUNNING;
+        this.obstacles = [];
+        this.speed = 6;
+        this.frame = 0;
+        this.backgrounds = [
+            new Background(skyBackgroundImg, 1, 1600, 600, canvas), 
+            new Background(mountainsBackgroundImg, 2, 1600, 600, canvas),
+            new Background(fartreesBackgroundImg, 3, 1600, 600, canvas),
+            new Background(treesBackgroundImg, 4, 1600, 800, canvas),
+            new Background(treesForegroundImg, 5, 1600, 800, canvas)
+        ]
     }
 
     update() {
-        if(this.status == GAMESTATUS.RUNNING) {
+        if (this.status == GAMESTATUS.RUNNING) {
             this.player.update();
+            handleBackgrounds(this);
+            handleObstacles(this);
+            this.frame++;
+            if (this.frame % 1000 == 0) {
+                this.speed++;
+            }
         }
-        if(this.status == GAMESTATUS.START){
-            
+        if (this.status == GAMESTATUS.START) {
+
         }
     }
 
--- js/main.js
+++ js/main.js
@@ -3,10 +3,10 @@ import Game from '/maggie-bird/js/game.js';
 const canvas = document.getElementById('maggie');
 const ctx = canvas.getContext('2d');
 
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+canvas.width = 1000;
+canvas.height = 800;
 
-const game = new Game(canvas);
+const game = new Game(canvas, ctx);
 
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
